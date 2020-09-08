bb3dcdaff54b7d4e319ca1cc7d071be3f0659709
Bernd Porr <mail@berndporr.me.uk>
2020-09-08 11:39:12 +0100

    Changed to AndroidX so that it's futureproof.

--- app/src/main/java/tech/glasgowneuro/ecg_axis/ECGAxisActivity.java
+++ app/src/main/java/tech/glasgowneuro/ecg_axis/ECGAxisActivity.java
@@ -2,14 +2,12 @@ package tech.glasgowneuro.ecg_axis;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 /**
  * The main activity
